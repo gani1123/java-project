c5d2dd5289a40880b664653ff6e17da5441b30e9
gani1123 <ganeshnalli1123@gmail.com>
2026-06-02 07:39:12 +0530

    Refactor dockerBuild function to use AWS ECR

--- dockerBuild.groovy
+++ dockerBuild.groovy
@@ -1,3 +1,11 @@
-def call(imageName) {
-    sh "docker build -t ${imageName} ."
+def call() {
+
+    def repository =
+        "${env.AWS_ACCOUNT_ID}.dkr.ecr.${env.AWS_REGION}.amazonaws.com/${env.APP_NAME}"
+
+    sh """
+        docker build \
+        -t ${repository}:${env.IMAGE_TAG} \
+        -t ${repository}:latest .
+    """
 }
