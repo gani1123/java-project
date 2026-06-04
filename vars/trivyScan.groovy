6fb25539802fa62251c9667222ce12d45b225f9a
gani1123 <ganeshnalli1123@gmail.com>
2026-06-04 11:14:42 +0530

    Add TMPDIR environment variable for Trivy scans
    Set TMPDIR for both Docker image and filesystem scans.

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -4,12 +4,14 @@ def call(String imageName = '', String imageTag = '') {
         // Docker image scan
         sh """
         echo "Running Trivy Image Scan..."
+        export TMPDIR=/opt/trivy-temp
         trivy image --severity HIGH,CRITICAL --exit-code 1 ${imageName}:${imageTag}
         """
     } else {
         // Filesystem scan
         sh """
         echo "Running Trivy FS Scan..."
+        export TMPDIR=/opt/trivy-temp
         trivy fs --severity HIGH,CRITICAL --exit-code 1 .
         """
     }
